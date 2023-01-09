7bf45be00dab11f12b14fffccbf344b334d402c2
atamayo <atamayoquintero@gmail.com>
2023-01-09 16:24:34 -0500

    feat: add show result by list

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".header__menu");
 const filter = document.querySelectorAll(".filter");
+const th = document.querySelector(".th");
+const bars = document.querySelector(".bars");
+const rs = document.querySelector("section");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("header__menu-visible");
 });
+
+th.addEventListener("click", () => {
+  rs.classList.add("result__search");
+  rs.classList.remove("result__search-bars");
+});
+
+bars.addEventListener("click", () => {
+  rs.classList.remove("result__search");
+  rs.classList.add("result__search-bars");
+});
+
 
 filter.forEach((item) => {
   item.addEventListener("click", () => {
@@ -21,4 +35,3 @@ filter.forEach((item) => {
 //     $(this).addClass('active')
 //   });
 // })
-
